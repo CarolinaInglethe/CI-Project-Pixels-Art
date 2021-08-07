61fdfbfa7778ecfce1709673f0666bb487480bb6
Carol_Inglethe <carolinainglethe@gmail.com>
2021-08-07 16:20:14 -0300

    quadro de pixels com 25 pixels

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ const pixelBoard = document.querySelector('#pixel-board');
 
 function createPixels() {
   for (let index = 0; index < 5; index += 1) {
-     const pixel = document.createElement('div');
-     pixel.className = 'pixel';
-     pixel.style.backgroundColor = 'white';
-     pixelBoard.appendChild(pixel)
+    const pixel = document.createElement('th');
+    pixel.className = 'pixel';
+    pixel.style.backgroundColor = 'white';
+    pixelBoard.appendChild(pixel)
+    for (let indexx = 1; indexx < 5; indexx += 1) {
+        const pixel = document.createElement('td');
+        pixel.className = 'pixel';
+        pixel.style.backgroundColor = 'white';
+        pixelBoard.appendChild(pixel)
+      }
   }
 }
 createPixels()
